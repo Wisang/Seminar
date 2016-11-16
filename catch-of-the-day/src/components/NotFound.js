8dec0bcc4ca08f8345b00225f3afe6565574ff0c
Wisang <wseom7@gmail.com>
2016-11-16 16:30:02 +0900

    until ch9

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,14 @@
+/**
+ * Created by wisangeom on 11/16/16.
+ */
+import React from 'react';
+
+class NotFound extends React.Component {
+    render() {
+        return (
+            <h1>Page does not exist!</h1>
+        );
+    }
+}
+
+export default NotFound;
