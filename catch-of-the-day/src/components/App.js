e3fafaf6af63f6923ae004381c52c94c7785b052
Wisang <wseom7@gmail.com>
2016-11-21 15:51:12 +0900

    ch13 complete

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,14 +4,32 @@ import Inventory from './Inventory';
 import Order from './Order';
 
 class App extends React.Component {
+    constructor() {
+        super();
+
+        this.addFish = this.addFish.bind(this);
+
+        this.state = {
+            fishes: {},
+            order: {}
+        };
+    }
+
+    addFish(fish) {
+        const fishes = {...this.state.fishes};
+        const timestamp = Date.now();
+        fishes[`fish-${timestamp}`] = fish;
+        this.setState({fishes});
+    }
+
     render() {
         return (
         <div className="catch-of-the-day">
             <div className="menu">
                 <Header tagline="FRESH SEAFOOD MARKET" />
             </div>
-            <Inventory amount="500"/>
             <Order />
+            <Inventory addFish={this.addFish}/>
         </div>
             );
     }
