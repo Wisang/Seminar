a4e756658a0b005759df39258c16cfd0b96be37f
Wisang <wseom7@gmail.com>
2016-11-22 10:59:22 +0900

    ch16 complete

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -7,7 +7,9 @@ import {formatPrice} from '../helpers'
 class Fish extends React.Component {
 
     render() {
-        const {details} = this.props;
+        const {details, index} = this.props;
+        const isAvailable = details.status === 'available';
+        const buttonText = isAvailable? 'Add to Order' : 'Sold Out';
         return (
             <li className="menu-fish">
                 <img src = {details.image} alt={details.name} />
@@ -16,7 +18,8 @@ class Fish extends React.Component {
                     <span className="price">{formatPrice(details.price)}</span>
                 </h3>
                 <p>{details.desc}</p>
-                <button>Add to Order</button>
+                <button onClick={() => this.props.addToOrder(index)}
+                        disabled={!isAvailable}>{buttonText}</button>
             </li>
         )
     }
